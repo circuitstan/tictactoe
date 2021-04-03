bee276b5b8684c93cc4c669454c3a08328ecbff7
Stenver Suurkütt <stenver.ss@gmail.com>
2021-04-03 22:20:06 +0300

    set up skeletons and main factory functions/modules

---
+++ script.js
@@ -0,0 +1,18 @@
+const gameBoard = (() => {
+    let x = ""
+    let y = ""
+    let gameBoard = ["X", "0", "X", "0", "X", "0", "X", "0", "X"]
+    return {
+        gameBoard
+    }
+})()
+
+const displayController = (() => {
+
+})()
+
+const player = () => {
+    
+}
+
+const container = document.getElementById('container')
