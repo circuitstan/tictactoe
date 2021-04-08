4b089d7270ecd8ec512870c834eef5c2bb77f0c3
Stenver Suurkütt <stenver.ss@gmail.com>
2021-04-08 20:14:49 +0300

    added marker selection by click and wrote function to determine game winner

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const gameBoard = (() => {
-    let selections = ["X", "0", "X", "0", "X", "0", "X", "0", "X"]
-    //let selections = []
+    let selections = []
     return {
         selections
     }
@@ -8,12 +7,106 @@ const gameBoard = (() => {
 
 const displayController = (() => {
     let mark = gameBoard.selections
-    for (let i = 0; i < mark.length; i++) {
-        let div = document.getElementById(i)        
-        div.innerText = mark[i]
-    }
+    const acceptableClicks = ['0','1','2','3','4','5','6','7','8']
+    let diag1 = 0
+    let diag2 = 0
+    let row1 = 0
+    let row2 = 0
+    let row3 = 0
+    let col1 = 0
+    let col2 = 0
+    let col3 = 0
     addEventListener('click', (e) => {
-        console.log(e)
+        let click = e.target.id
+        if (mark.includes(click)) {
+        } else  if (acceptableClicks.includes(click)) {
+            mark.push(click)
+            for (let i = 0; i < mark.length; i++) {
+                let div = document.getElementById(click)            
+                if (mark.length % 2 != 0) {
+                    div.innerText = "X"
+                } else {
+                    div.innerText = "O"
+                }
+            }
+            if (mark.length % 2 != 0) {
+                if (click == '0') {
+                    row1 += 1
+                    col1++
+                    diag1++
+                } else if (click == '1') {
+                    row1 += 1
+                    col2++
+                } else if (click == '2') {
+                    row1 += 1
+                    col3++
+                    diag2++
+                } else if (click == '3') {
+                    row2++
+                    col1++
+                } else if (click == '4') {
+                    row2++
+                    col2++
+                    diag1++
+                    diag2++
+                } else if (click == '5') {
+                    row2++
+                    col3++
+                } else if (click == '6') {
+                    row3++
+                    col1++
+                    diag2++
+                } else if (click == '7') {
+                    row3++
+                    col2++
+                } else if (click == '8') {
+                    row3++
+                    col3++
+                    diag1++
+                }
+            } else {
+                if (click == '0') {
+                    row1--
+                    col1--
+                    diag1--
+                } else if (click == '1') {
+                    row1--
+                    col2--
+                } else if (click == '2') {
+                    row1--
+                    col3--
+                    diag2--
+                } else if (click == '3') {
+                    row2--
+                    col1--
+                } else if (click == '4') {
+                    row2--
+                    col2--
+                    diag1--
+                    diag2--
+                } else if (click == '5') {
+                    row2--
+                    col3--
+                } else if (click == '6') {
+                    row3--
+                    col1--
+                    diag2--
+                } else if (click == '7') {
+                    row3--
+                    col2--
+                } else if (click == '8') {
+                    row3--
+                    col3--
+                    diag1--
+                }
+            }
+            console.log(click)
+            if ((row1 == 3) || (row2 == 3) || (row3 == 3) || (col1 == 3) || (col2 == 3) || (col3 == 3) || (diag1 == 3) || (diag2 == 3)) {
+                alert("X wins")
+            } else if ((row1 == -3) || (row2 == -3) || (row3 == -3) || (col1 == -3) || (col2 == -3) || (col3 == -3) || (diag1 == -3) || (diag2 == -3)) {
+                alert("O wins")
+            }
+        }
         })
 })()
 
