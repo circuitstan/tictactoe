4ae8a61de2f5b398d236278591cc3893be99f1dc
Stenver Suurkütt <stenver.ss@gmail.com>
2021-04-14 17:06:02 +0300

    added draw ending and option for players to enter names

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const displayController = (() => {
     let col1 = 0
     let col2 = 0
     let col3 = 0
+    let count = 0
     addEventListener('click', (e) => {
         let click = e.target.id
         if (mark.includes(click)) {
@@ -34,88 +35,115 @@ const displayController = (() => {
                     row1 += 1
                     col1++
                     diag1++
+                    count++
                 } else if (click == '1') {
                     row1 += 1
                     col2++
+                    count++
                 } else if (click == '2') {
                     row1 += 1
                     col3++
                     diag2++
+                    count++
                 } else if (click == '3') {
                     row2++
                     col1++
+                    count++
                 } else if (click == '4') {
                     row2++
                     col2++
                     diag1++
                     diag2++
+                    count++
                 } else if (click == '5') {
                     row2++
                     col3++
+                    count++
                 } else if (click == '6') {
                     row3++
                     col1++
                     diag2++
+                    count++
                 } else if (click == '7') {
                     row3++
                     col2++
+                    count++
                 } else if (click == '8') {
                     row3++
                     col3++
                     diag1++
+                    count++
                 }
             } else {
                 if (click == '0') {
                     row1--
                     col1--
                     diag1--
+                    count++
                 } else if (click == '1') {
                     row1--
                     col2--
+                    count++
                 } else if (click == '2') {
                     row1--
                     col3--
                     diag2--
+                    count++
                 } else if (click == '3') {
                     row2--
                     col1--
+                    count++
                 } else if (click == '4') {
                     row2--
                     col2--
                     diag1--
                     diag2--
+                    count++
                 } else if (click == '5') {
                     row2--
                     col3--
+                    count++
                 } else if (click == '6') {
                     row3--
                     col1--
                     diag2--
+                    count++
                 } else if (click == '7') {
                     row3--
                     col2--
+                    count++
                 } else if (click == '8') {
                     row3--
                     col3--
                     diag1--
+                    count++
                 }
             }
             console.log(click)
             if ((row1 == 3) || (row2 == 3) || (row3 == 3) || (col1 == 3) || (col2 == 3) || (col3 == 3) || (diag1 == 3) || (diag2 == 3)) {
-                alert("X wins")
+                alert(player1.name + " wins")
             } else if ((row1 == -3) || (row2 == -3) || (row3 == -3) || (col1 == -3) || (col2 == -3) || (col3 == -3) || (diag1 == -3) || (diag2 == -3)) {
-                alert("O wins")
+                alert(player2.name + " wins")
+            } else if (count == 9) {
+                alert("It's a draw!")
+                console.log("draw")
             }
         }
         })
 })()
 
-const player = (marker) => {
+const player = (marker, name) => {
     
     return {
-        marker
+        marker,
+        name
     }
 }
 
-const player1 = player("X")
-const player2 = player("0")
+const name1 = prompt("Enter name for Player 1: ")
+const name2 = prompt("Enter name for Player 2: ")
+
+const player1 = player("X", name1)
+const player2 = player("0", name2)
+
+console.log(player1.marker)
